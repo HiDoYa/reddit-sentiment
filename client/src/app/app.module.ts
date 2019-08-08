ef6b20df6507fab47003118489f11a2ff27ac5c6
HiDoYa <hiroyagojo@gmail.com>
2019-08-07 22:05:59 -0700

    Outline structure and attempt bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SearchBarComponent } from "./components/search-bar/search-bar.component";
+import { OptionsComponent } from "./components/options/options.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SearchBarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, SearchBarComponent, OptionsComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
