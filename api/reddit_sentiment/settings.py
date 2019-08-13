0a792d662ef7e185a4e589cafc64b83e3e6e2b2a
HiDoYa <hiroyagojo@gmail.com>
2019-08-12 22:02:01 -0700

    Started on django backend

--- api/reddit_sentiment/settings.py
+++ api/reddit_sentiment/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'reddit_sentiment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
